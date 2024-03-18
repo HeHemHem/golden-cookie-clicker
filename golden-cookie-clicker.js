f4d92d294fb8fb66f3ccc95d843d635f00ce3034
JK <54625393+HeHemHem@users.noreply.github.com>
2024-03-18 23:01:06 +0100

    Update golden-cookie-clicker.js

--- golden-cookie-clicker.js
+++ golden-cookie-clicker.js
@@ -1,7 +1,34 @@
-setInterval(function() {
+// ==/UserScript==
 
-Game.shimmers.forEach(function(shimmer) {
-if (shimmer.type == "golden") { shimmer.pop() }
-})
+/* global Game:false, GM_info:false */
+function executeAutoActions() {
+  if (Game.T % (Game.fps * 0.5) !== 0) {
+    // Game logic ticks very fast, only trigger every 0.5s
+    return;
+  }
 
-}, 500);
+  // Click all golden cookies
+  Game.shimmers.forEach(obj => obj.pop());
+
+  // Harvest sugar lumps
+  if ((new Date() - Game.lumpT) > Game.lumpRipeAge) {
+    Game.harvestLumps(1);
+  }
+  
+function installHelper() {
+  // Startup notification
+  let version = GM_info.script.version;
+  note(`Version ${version} loaded.`);
+
+  // Do not click toggle upgrades
+  blockingUpgrades = blockingUpgrades.concat(Game.UpgradesByPool['toggle'].map(obj => obj.id));
+
+  Game.customLogic.push(executeAutoActions);
+}
+
+function note(msg, quick = true) {
+  // Icon: img/icons.png 0-based indices
+  Game.Notify('Auto-CookieClicker', msg, [12, 0], quick, true);
+}
+
+(() => window.setTimeout(installHelper, 1000))();
