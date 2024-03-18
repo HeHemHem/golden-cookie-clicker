64a50d02056c3dbba86c1f5beb0a7b0813d33aca
JK <54625393+HeHemHem@users.noreply.github.com>
2024-03-18 20:00:08 +0100

    Update golden-cookie-clicker.js

--- golden-cookie-clicker.js
+++ golden-cookie-clicker.js
@@ -1,48 +1,7 @@
-// Golden Cookie Clicker
-// By RainSlide
-// Automatically click golden cookies and reindeers, won't click wrath cookies.
-// Version 1.0
-// Updated 2022-04-19
+setInterval(function() {
 
-// Cookie Clicker: https://orteil.dashnet.org/cookieclicker/
+Game.shimmers.forEach(function(shimmer) {
+if (shimmer.type == "golden") { shimmer.pop() }
+})
 
-"use strict";
-
-if (typeof Game !== "object" || Game === null || !Array.isArray(Game.shimmers)) {
-
-	console.error(
-		"Golden Cookie Clicker: Cookie Clicker API is not ready or invalid.\n" +
-		"Please make sure you are running this script on a Cookie Clicker webpage, " +
-		"and the page is fully loaded."
-	);
-
-} else if (typeof Proxy !== "function") {
-
-	console.error(
-		"Golden Cookie Clicker: JavaScript Proxy API is not available, " +
-		"either update your browser, or use the ES3 compatible version:\n"+
-		"https://rainslide.neocities.org/cookieclicker/GoldenCookieClicker.es3.js"
-	);
-
-} else {
-
-	const apply = (target, _this, args) => {
-		var shimmer = args[0];
-		if (shimmer.type === "Wrath cookie" || shimmer.type === "reindeer") {
-			setTimeout(() => shimmer.pop(), 500);
-		}
-		return Reflect.apply(target, _this, args);
-	};
-
-	Object.defineProperty(Game.shimmers, "push", {
-		value: new Proxy(Game.shimmers.push, { apply }),
-		writable: true,
-		enumerable: false,
-		configurable: true
-	});
-
-	typeof Game.Win === "function" &&
-	Game.Win("Third-party");
-
-
-}
+}, 500);
